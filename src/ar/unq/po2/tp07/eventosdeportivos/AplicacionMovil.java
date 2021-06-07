319bfcf007c8dd5ca72b0c88bc9714ae77ffa9e7
davedev <davidcarrevedo@gmail.com>
2021-06-07 15:23:39 -0300

    base implementada

---
+++ src/ar/unq/po2/tp07/eventosdeportivos/AplicacionMovil.java
@@ -0,0 +1,15 @@
+package ar.unq.po2.tp07.eventosdeportivos;
+
+public class AplicacionMovil implements Suscriptor {
+
+	public AplicacionMovil() {
+		
+	}
+
+	@Override
+	public void recibirEventoDeportivo(EventoDeportivo eventoDeportivo) {
+		
+
+	}
+
+}
---
+++ src/ar/unq/po2/tp07/eventosdeportivos/Suscriptor.java
@@ -0,0 +1,7 @@
+package ar.unq.po2.tp07.eventosdeportivos;
+
+public interface Suscriptor {
+	
+	public void recibirEventoDeportivo(EventoDeportivo eventoDeportivo);
+
+}
