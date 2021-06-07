747438755d400e649bbf28b4600e5e7bc2887250
davedev <davidcarrevedo@gmail.com>
2021-06-07 08:51:03 -0300

    faltan criterios de busqueda

---
+++ src/ar/unq/po2/tp07/publicaciones/Criterio.java
@@ -0,0 +1,7 @@
+package ar.unq.po2.tp07.publicaciones;
+
+public interface Criterio {
+	
+	public Boolean esDeInteres(Articulo articulo);
+
+}
---
+++ src/ar/unq/po2/tp07/publicaciones/Suscriptor.java
@@ -0,0 +1,7 @@
+package ar.unq.po2.tp07.publicaciones;
+
+public interface Suscriptor {
+	
+	public void actualizar(Articulo articulo);
+
+}
