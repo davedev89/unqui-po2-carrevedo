6898a35d531989e837ac519abd85dbc557658f4f
davedev <davidcarrevedo@gmail.com>
2021-06-17 13:38:51 -0300

    cultivos clases hechas falta testear

---
+++ src/ar/unq/po2/tp07/cultivos/Cultivo.java
@@ -0,0 +1,13 @@
+package ar.unq.po2.tp07.cultivos;
+
+public abstract class Cultivo {
+	
+	public Cultivo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public abstract Float getGanancia();
+	
+	//se prueba el uso de float, pero originalmente se iba a utilizar double
+
+}
